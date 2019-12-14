af782de7883bc60d0ae9536b30dc5e2be74fabee
Rochan Avlur Venkat <rochan.avlur@gmail.com>
2019-12-14 14:46:14 +0530

    Added rgb and segm support

--- baselines/common/atari_wrappers.py
+++ baselines/common/atari_wrappers.py
@@ -8,6 +8,13 @@ import cv2
 cv2.ocl.setUseOpenCL(False)
 from .wrappers import TimeLimit
 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+from skimage import segmentation
+import skimage.color as color
 
 class NoopResetEnv(gym.Wrapper):
     def __init__(self, env, noop_max=30):
@@ -131,8 +138,42 @@ class ClipRewardEnv(gym.RewardWrapper):
         return np.sign(reward)
 
 
+class Usegm(nn.Module):
+    def __init__(self, input_dim, nChannel, nConv):
+        super(Usegm, self).__init__()
+        self.conv1 = nn.Conv2d(input_dim, nChannel, kernel_size=3, stride=1, padding=1 )
+        self.bn1 = nn.BatchNorm2d(nChannel)
+        self.conv2 = []
+        self.bn2 = []
+        self.nConv = nConv
+        for i in range(nConv-1):
+            self.conv2.append( nn.Conv2d(nChannel, nChannel, kernel_size=3, stride=1, padding=1 ) )
+            self.bn2.append( nn.BatchNorm2d(nChannel) )
+        self.conv3 = nn.Conv2d(nChannel, nChannel, kernel_size=1, stride=1, padding=0 )
+        self.bn3 = nn.BatchNorm2d(nChannel)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu( x )
+        x = self.bn1(x)
+        for i in range(self.nConv-1):
+            x = self.conv2[i](x)
+            x = F.relu( x )
+            x = self.bn2[i](x)
+        x = self.conv3(x)
+        x = self.bn3(x)
+        return x
+
+# Initialize default weights to the frame segmentation network
+def init_weights(m):
+    if type(m) == nn.Conv2d:
+        m.weight.data.fill_(torch.tensor(0.5))
+        m.bias.data.fill_(torch.tensor(0))
+
 class WarpFrame(gym.ObservationWrapper):
-    def __init__(self, env, width=84, height=84, grayscale=True, dict_space_key=None):
+    def __init__(self, env, width=84, height=84, grayscale=True,
+                    dict_space_key=None, use_rgb=False, use_segm=False,
+                    use_unsup_segm=False):
         """
         Warp frames to 84x84 as done in the Nature paper and later work.
 
@@ -144,10 +185,22 @@ class WarpFrame(gym.ObservationWrapper):
         self._height = height
         self._grayscale = grayscale
         self._key = dict_space_key
-        if self._grayscale:
+
+        self.use_rgb = use_rgb
+        self.use_segm = use_segm
+        self.use_unsup_segm = use_unsup_segm
+
+        if use_unsup_segm:
+            self.segm_model = Usegm(3, 100, 3)
+            self.optimizer_USOD = optim.SGD(self.segm_model.parameters(), lr=0.1, momentum=0.9)
+
+            self.count = 0
+
+        if self.use_rgb or self.use_segm or self.use_unsup_segm:
             num_colors = 3
+            self._grayscale = False
         else:
-            num_colors = 3
+            num_colors = 1
 
         new_space = gym.spaces.Box(
             low=0,
@@ -155,6 +208,86 @@ class WarpFrame(gym.ObservationWrapper):
             shape=(self._height, self._width, num_colors),
             dtype=np.uint8,
         )
+
+        if self._key is None:
+            original_space = self.observation_space
+            self.observation_space = new_space
+        else:
+            original_space = self.observation_space.spaces[self._key]
+            self.observation_space.spaces[self._key] = new_space
+        assert original_space.dtype == np.uint8 and len(original_space.shape) == 3
+
+    def observation(self, obs):
+        if self._key is None:
+            frame = obs
+        else:
+            frame = obs[self._key]
+
+        frame_g = cv2.resize(
+            frame, (self._width, self._height), interpolation=cv2.INTER_AREA
+        )
+
+        if not self.use_rgb:
+
+            if self.use_segm:
+                frame_slic = segmentation.slic(frame_g, n_segments=5)
+                frame_g = color.label2rgb(frame_slic, frame_g, kind='avg')
+
+#            if self.use_unsup_segm:
+#                if self.count == 5:
+#                    self.segm_model.apply(init_weights)
+#                    optimizer_USOD.zero_grad()
+#                    self.count = 0
+#                self.count+=1
+#
+#                # Scale the image x4 times
+#                r = cv2.resize(frame_g, \
+#                        (frame_g.shape[1]*4, frame_g.shape[0]*4), \
+#                        interpolation=cv2.INTER_NEAREST)
+#
+#                st = im_preprocess(r[:][:][:], 10, 1000)
+#
+#                # Segment the frame
+#                im_target_rgb = train_segmentation(self.segm_model, st, \
+#                        loss_fn, optimizer_USOD, \
+#                        label_colours, args)
+
+            if self._grayscale:
+                frame_g = cv2.cvtColor(frame_g, cv2.COLOR_RGB2GRAY)
+                frame_g = np.expand_dims(frame_g, -1)
+
+        if self._key is None:
+            obs = frame_g
+        else:
+            obs = obs.copy()
+            obs[self._key] = frame_g
+        return obs
+
+# TODO
+class WarpFrame_feov(gym.ObservationWrapper):
+    def __init__(self, env, width=84, height=84, grayscale=True,
+                    dict_space_key=None):
+        """
+        Warp frames to 84x84 as done in the Nature paper and later work.
+
+        If the environment uses dictionary observations, `dict_space_key` can be specified which indicates which
+        observation should be warped.
+        """
+        super().__init__(env)
+        self._width = width
+        self._height = height
+        self._grayscale = grayscale
+        self._key = dict_space_key
+
+        num_colors = 1
+
+        new_space = gym.spaces.Box(
+            low=0,
+            high=255,
+            shape=(self._height, self._width, num_colors),
+            dtype=np.uint8,
+        )
+
         if self._key is None:
             original_space = self.observation_space
             self.observation_space = new_space
@@ -186,7 +319,6 @@ class WarpFrame(gym.ObservationWrapper):
             obs = obs.copy()
             obs[self._key] = frame_g
         return obs
-
 
 class FrameStack(gym.Wrapper):
     def __init__(self, env, k):
@@ -275,15 +407,24 @@ def make_atari(env_id, max_episode_steps=None):
         env = TimeLimit(env, max_episode_steps=max_episode_steps)
     return env
 
-def wrap_deepmind(env, episode_life=True, clip_rewards=True, frame_stack=False, scale=False):
+def wrap_deepmind(env, episode_life=True, clip_rewards=True, frame_stack=False,
+                            scale=False, use_rgb=False, use_segm=False,
+                                    use_unsup_segm=False, use_feov=False):
     """Configure environment for DeepMind-style Atari.
     """
     if episode_life:
         env = EpisodicLifeEnv(env)
     if 'FIRE' in env.unwrapped.get_action_meanings():
         env = FireResetEnv(env)
-    # Dont change the shape of the frame right now, but make BW and RGB
-    env = WarpFrame(env)
+
+    if use_feov:
+        env = WarpFrame_feov(env)
+    elif (use_rgb or use_segm or use_unsup_segm):
+        env = WarpFrame(env, use_rgb=use_rgb, use_segm=use_segm,
+                use_unsup_segm=use_unsup_segm)
+    else:
+        env = WarpFrame(env)
+
     if scale:
         env = ScaledFloatFrame(env)
     if clip_rewards:
